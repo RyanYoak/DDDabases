6c90b33cda6d1cb41c75f37cef254266495a2cbc
blam0101 <blam1@kent.edu>
2019-12-03 12:06:00 -0500

    Fixed some syntax for editLogs

--- editLog.php
+++ editLog.php
@@ -6,12 +6,12 @@
     if(isset($_GET["edit"])){
       $id = $_GET["edit"];
       $log_date = $GET["log_date"];
-      $sql = "SELECT * FROM logs WHERE employee_id = '$id' AND log_date = '$log_date';
-        $empArray = $conn->query($sql) or die($conn->error);
-        $row = $empArray->fetch_array();
-        $login_time = $row['login_time'];
-        $logout_time = $row['logout_time'];
-        $log_date = $row['log_date'];
+      $sql = "SELECT * FROM logs WHERE employee_id = $id AND log_date = $log_date";
+      $empArray = $conn->query($sql) or die($conn->error);
+      $row = $empArray->fetch_array();
+      $login_time = $row['login_time'];
+      $logout_time = $row['logout_time'];
+      $log_date = $row['log_date'];
     }
 
     if(isset($_POST["update"])){
@@ -20,7 +20,7 @@
       $log_date - $_POST['log_date'];
 
       $mySQL= "UPDATE logs
-               SET login_time='$login_time', logout_time='$logout_time', log_date='log_date',
+               SET login_time='$login_time', logout_time='$logout_time', log_date='$log_date',
                WHERE employee_id = $id;";
 
       $conn->query($mySQL);
